35e2550b65c9d151795c36693022d0acb705a63d
smugytea4real <thomasandromeda999@gmail.com>
2024-10-29 10:45:38 +0700

    Update showing image and product from db.json

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
-import { Hasil, ListCategories, NavbarComponent } from './components';
+import { Hasil, ListCategories, Menus, NavbarComponent } from './components';
 import { Row, Col, Container } from 'react-bootstrap';
+import React, { Component } from 'react'
+import { API_URL } from './utils/constants';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <div className='mt-3'>
-        <Container fluid>
-        <Row>
-          <ListCategories />
-          <Col>
-            <h4>
-              <strong>Daftar Produk</strong>
-            </h4>
-            <hr />
-          </Col>
-          <Hasil />
-        </Row>
-        </Container>
-      </div>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       menus : [],
+    }
+  }
+
+componentDidMount(){
+  axios.get(API_URL+"products")
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log(error);
+      })
 }
 
-export default App;
+  render() {
+    const { menus } = this.state
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className='mt-3'>
+          <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Produk</strong>
+              </h4>
+              <hr />
+              <Row>
+                {menus && menus.map((menu) => (
+                  <Menus 
+                    key={menu.id}
+                    menu={menu}
+                  />
+                ))}
+              </Row>
+            </Col>
+            <Hasil />
+          </Row>
+          </Container>
+        </div>
+      </div>
+    )
+  }
+}
+
